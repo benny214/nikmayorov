a81d8b9cbfb787171c5e5f7247e31f0af1b523da
Benny214 <benny.moralez@gmail.com>
2016-04-13 08:42:07 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,65 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
+
+//slick
+$('.slider__list').slick({
+  //dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 2,
+  centerMode: true,
+  variableWidth: true,
+  centerPadding: 0,
+  autoplay: true
+});
+
+//sticky header
+$(document).ready(function(){
+  $("#nav").sticky({topSpacing:0});
+});
